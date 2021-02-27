fb6ad34cc090a76e03e55beaa78c6b9bbdb35267
anthonyluna <anthonyluna@ucsb.edu>
2021-02-26 17:47:24 -0800

    Finalized code with many many changes. Most importantly, it is now clearly annotated.

--- R/ricker.R
+++ R/ricker.R
@@ -1,34 +1,92 @@
+# Goal: To model the population dynamics of two competing species, one that has
+# a life phase difference and the other that doesn't. This function uses a
+# Adult/Juvinile Ricker model, This is a discrete competition model and
+# therefore can benefit from the use of a while loop. For more on the model,
+# see:
+
+# Cushing, J.M., Sheree Levarge, Nakul Chitnis, and Shandelle M. Henson. “Some
+# Discrete Competition Models and the Competitive Exclusion Principle †.”
+# Journal of Difference Equations and Applications 10, no. 13–15 (November
+# 2004): 1139–51. https://doi.org/10.1080/10236190410001652739.
+
+# Inputs: The initial count of juveniles and adults of species x and species y.
+#
+# Parameters: Juvenile death rate (u), competition coefficients (c11, c12, c21,
+# c22), and outputs
+#
+# Output: Population of species x and species y at timestep i
+#
+# Modules: 
+#   - Initialize the outputed dataframe
+#   - Use initial counts for first timestep calculation 
+#   - Loop discrete function iteration
+#   - Store and return results at the end
+library(checkmate)
 ricker <- function(A0, J0, u, y0, a, b, c11, c12, c21, c22) {
+  # Input Validation/Error Checking
+
+  checkmate::assert_number(A0,lower = 0)
+  checkmate::assert_number(J0,lower = 0)
+  checkmate::assert_number(u,lower = 0, upper = 1)
+  checkmate::assert_number(y0,lower = 0)
+  checkmate::assert_number(a,lower = 0)
+  checkmate::assert_number(b,lower = 0)
+  checkmate::assert_number(c11,lower = 0)
+  checkmate::assert_number(c12,lower = 0)
+  checkmate::assert_number(c21,lower = 0)
+  checkmate::assert_number(c22,lower = 0)
+  
+  
+  # Initializing the while loop iterator and the output dataframe
   i <- 1
   timeseries <- data.frame(
     "i" = i,
     "xn" = A0 + J0,
     "yn" = y0
   )
+  
+  # Constructing the while loop. We assume 1000 iterations will show periodic
+  # behavior.
+  
   while (i < 1000) {
+    
     if (i == 1) {
+      
+      # Using the initial population measures
+      # Juvenile growth rate
       Jn <- a * A0 * exp(-c11 * A0 - c12 * y0)
+      # Adult growth rate as it relates to the die off of juveniles
       An <- (1 - u) * J0
+      # Population of species y
       yn <- b * y0 * exp(-c21 * J0 - c22 * y0)
+      # Population of species x including both adults and juveniles
       xn <- A0 + J0
       new_row <- c(i, xn, yn)
+      
     } else {
+      
+      # Now using the discrete iteratative timestep function for growth. Same
+      # equations as described above. Values are written to a place holder so
+      # that values are calculated for each at the same timestep.
+      
       Jnn <- a * An * exp(-c11 * An - c12 * yn)
       Ann <- (1 - u) * Jn
       ynn <- b * yn * exp(-c21 * Jn - c22 * yn)
       xnn <- An + Jn
-
+      
+      # Dummy storage to used storage
       Jn <- Jnn
       An <- Ann
       yn <- ynn
       xn <- xnn
-
+      
       new_row <- c(i, xn, yn)
     }
-
+    
+    # Append the timestep to the output dataframe
     timeseries <- rbind(timeseries, new_row, make.row.names = FALSE)
 
-
+    # Incrementor
     i <- i + 1
   }
   return(timeseries)
--- R/ricker_outcome.R
+++ R/ricker_outcome.R
@@ -1,29 +1,129 @@
-ricker_outcome <- name <- function(competition_data) {
-  outcome <-
-    unique(round(tail(competition_data, n = 20) %>% select(-i), 0))
-  
-  outcome_title <-
-    case_when(
-      nrow(outcome) > 1 ~ "Periodic Population Cycle",
+# Outcome of the Adult/Juvenile Ricker Model
+#
+# Goal: To provide two plots that describes the population dynamics of the 
+# two species of interest given the inputs to the ricker function (see ricker.R)
+#
+# Input: A dataframe with 3 columns that match the output of the ricker function
+#
+# parameters: Name of species x and species y
+#
+# output: two plots with a title describing what happened.
+#
+# Modules:
+#   - Find the outcome
+#   - Build the names
+#   - Choose the name based on the outcome
+#   - Plot the change over time
+#   - Plot the population phase plot
+#   - Combine the plots and title into one image and return this
+
+
+library(patchwork)
+library(checkmate)
+
+ricker_outcome <-
+  function(competition_data,
+           species_x_name = "X",
+           species_y_name = "Y") {
+    
+    # Error checking, make sure that the dataframe is the expected format
+    # and that the names given as arguments are strings
+    checkmate::assert_data_frame(competition_data, ncols = 3)
+    checkmate::assert_true(identical(names(competition_data),c("i","xn","yn")))
+    checkmate::assert_string(species_x_name)
+    checkmate::assert_string(species_y_name)
+    
+    # Generate the outcome. This is a heuristic that looks at the last 20
+    # timesteps from the competition_data, rounds to the 4th decimal place for
+    # each (this is where the heuristic is most vulnerable, but for most cases,
+    # systems will reach equilibrium after 1000 iterations), and gathers the
+    # unique values.
+    
+    outcome <-
+      unique(round(tail(competition_data, n = 20) %>% select(-i), 4))
+    
+    # Defining the different ploit titles based on outcomes
+    outcome_periodic <- "Periodic Population Cycle"
+    
+    outcome_x_extinct <-
+      paste(species_y_name,
+             "Survives,",
+             species_x_name,
+             "Goes Extinct")
+    
+    outcome_y_extinct <-
+      paste(species_x_name,
+             "Survives,",
+             species_y_name,
+             "Goes Extinct")
+    
+    outcome_x_dom <-
+      paste("Both species survive,",
+             species_x_name,
+             "with higher population.")
+    
+    outcome_y_dom <-
+      paste("Both species survive,",
+             species_y_name,
+             "with higher population.")
+    
+    # Testing outcomes
+    outcome_title <-
+      case_when(
+        # if there is more than 1 row aver the hueristic is applied, this is 
+        # likely a periodic outcome. 
+      nrow(outcome) > 1 ~ outcome_periodic,
+      # x extinction scenario
       nrow(outcome) == 1 &
-        outcome$xn == 0 ~ "Species Y Outcompetes Species X",
+        outcome$xn == 0 ~ outcome_x_extinct,
+      
+      # y extinction scenario
       nrow(outcome) == 1 &
-        outcome$yn == 0 ~ "Species X Outcompetes Species Y"
+        outcome$yn == 0 ~ outcome_y_extinct,
+      
+      # cohabitation with x dominating
+      nrow(outcome) == 1 &
+        outcome$yn != 0 &
+        outcome$xn != 0 &
+        outcome$yn < outcome$xn ~ outcome_x_dom,
+      
+      # cohabitation with y dominating
+      nrow(outcome) == 1 &
+        outcome$yn != 0 &
+        outcome$xn != 0 &
+        outcome$yn > outcome$xn ~ outcome_y_dom
     )
   
   
-  change_over_time <-
-    ggplot(data = head(competition_data, n = 100), aes(x = i)) +
-    geom_line(aes(y = xn), color = "red") +
-    geom_line(aes(y = yn), color = "blue") +
-    geom_point(aes(y = xn), color = "red") +
-    geom_point(aes(y = yn), color = "blue") +
-    theme_minimal()
+    # Here is the plot over time. Only take the first 100 rows, its pretty
+    # boring after that normally. Need to pivot data to make the legends play
+    # nicely.
+    change_over_time <-
+      ggplot(
+        data = head(competition_data, n = 100) %>%
+          pivot_longer(
+            cols = c("xn", "yn"),
+            names_to = "Species",
+            values_to = "Population"
+          ),
+        aes(x = i)
+      ) +
+      geom_line(aes(y = Population, color = species) )+
+      geom_point(aes(y = Population, color = species)) +
+      scale_x_continuous(name = "Population") +
+      scale_y_continuous(name = "Timestep") +
+      scale_color_discrete(labels = c(species_x_name, species_y_name)) +
+      theme_minimal()
   
-  population_point <- ggplot(data = competition_data) +
+  # Population phase plot here. This may be the not so great way to do this, but
+  # it works for now
+  population_point <- ggplot(data = competition_data %>% filter(i%%10==0)) +
     geom_point(aes(x = xn, y = yn, alpha = i)) +
+    scale_x_continuous(name=species_x_name)+
+    scale_y_continuous(name=species_y_name)+
     theme_minimal()
   
+  # Formatting the output plot with the outcomes.
   return((change_over_time / population_point) +
            plot_annotation(title = paste("Outcome:", unique(outcome_title))))
 }
