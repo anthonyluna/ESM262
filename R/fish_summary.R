e078899a462f4dd6ae0640545d358714101e38d1
anthonyluna <anthonyluna@ucsb.edu>
2021-03-02 16:04:31 -0800

    annotations and a small refactor in fish_summary

--- R/fish_summary.R
+++ R/fish_summary.R
@@ -1,12 +1,12 @@
 # Fish summary function
-# input: vector of fish names
-# parameter: to return or not return a histogram of the fish types
-# output: The most common fish, the rarest fish, the total number of fish
+# Input: Vector of fish names
+# Parameter: To return or not return a histogram of the fish types
+# Output: The most common fish, the rarest fish, the total number of fish
 ##
 
 library(checkmate)
 
-fish_summary <- function(fish_list, histogram = FALSE) {
+fish_summary <- function(fish_list, fishtogram = FALSE) {
   # Validation checking
   # is the input an atomic vector?
   assert_atomic_vector(fish_list)
@@ -15,21 +15,32 @@ fish_summary <- function(fish_list, histogram = FALSE) {
   # wrong data since we normally call fish by name not number.
   check_character(fish_list)
   
+  # Change the input vector to a factor data type
   factorized_fish_list <- as.factor(fish_list)
+  
+  # output the summary
   fish_summary <- summary(factorized_fish_list)
   
+  # Most common fish from the input
   most_common_fish <- names(which.max(fish_summary))
+  # Rarest fish from the input
   rarest_fish <- names(which.min(fish_summary))
+  # Total number of fish from the input
   total_fish <- sum(fish_summary)
   
-  if (histogram == TRUE) {
+  # Logic to control the output of a histogram
+  if (fishtogram == TRUE) {
+    # Build the plot title based on total fish
     plottitle = sprintf("We caught %d fish today!", total_fish)
     
+    # Create the fishtogram
     fishtogram <-
       ggplot(data.frame(fish = factorized_fish_list),
              aes(fish, fill = fish)) +
       geom_histogram(stat = "count") +
       labs(title = plottitle)
+    
+    # Return all the outputs in a list
     return(
       list(
         most_common_fish = most_common_fish,
@@ -39,6 +50,8 @@ fish_summary <- function(fish_list, histogram = FALSE) {
       )
     )
   } else {
+    
+    # User didn't want the fishtogram...
     return(
       list(
         most_common_fish = most_common_fish,
@@ -47,6 +60,4 @@ fish_summary <- function(fish_list, histogram = FALSE) {
       )
     )
   }
-  # you can also add numbers to the string
-  
 }
